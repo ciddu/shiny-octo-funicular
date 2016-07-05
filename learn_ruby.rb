87bd3f41acf26a41c9d3e352bf42e89a0fb9e0dd
Shashank Ciddu <ciddu@users.noreply.github.com>
2016-07-05 01:19:19 -0700

    Delete learn_ruby.rb

--- learn_ruby.rb
+++
@@ -1,8 +0,0 @@
-puts "Hello there!" # Because a comment is supposed to look like this in ruby.
-
-# using #{} is how we add a computation to text string.
-puts "Simple math operation #{2+5}"
-
-nitrogen = 78
-
-puts "Air has #{nitrogen}% nitrogen!"
